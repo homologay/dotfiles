d4111d622db209e3796a1cb39a43966e7f814419
maxine <maxbeckie@tutanota.com>
2023-02-01 19:26:28 -0800

    added some more things

--- init.sh
+++ init.sh
@@ -1,30 +1,39 @@
 #!/bin/sh
 
-#NOTE: needs to run as root
-
 # install curl
-xbps-install curl
+sudo xbps-install curl
 
 # install rustup
 curl --proto "=https" --tlsv1.3 https://sh.rustup.rs -sSf | sh
 
-# install cargo and components
-# todo!
+# install cargo  
+# rustup install clippy rust-analyzer, rustfmt, others,..
 
-# install alacritty, starship, kitty ... (terminal env)
-# todo!
+# check rustup --version command return code, to see if rustup got PATH configured right
+# if fail, return error message and try to set PATH to include .cargo/bin 
 
 # install nushell
 xbps-install nushell
 
-# make nushell default, other defaults..
-# todo!
 
+# (why not just install all the things needed for haskell development)
 # install X11 and xmonad, according to https://www.reddit.com/r/voidlinux/comments/mpdr5f/how_to_install_xmonad/ 
-
-xbps-install libX11-devel libXScrnSaver-devel libXft-devel libXinerama-devel libXrandr-devel
-xbps-install cabal-install xmobar
+sudo xbps-install libX11-devel libXScrnSaver-devel libXft-devel libXinerama-devel libXrandr-devel
+sudo xbps-install cabal-install xmobar
 cabal install --lib xmonad xmonad-contrib X11
 cabal install xmonad
 
-#add $HOME/.cabal/bin to PATH 
+#add $HOME/.cabal/bin to PATH
+# in nushell config: PATH=$PATH:$HOME/.cabal/bin
+
+# install alacritty, starship, kitty ... (terminal env)
+# todo!
+
+# install zellij, 
+
+# install and alias to GNU counterparts
+# bat=cat, exa=ls, dust=du, rg, bottom=top, tldr (tealdeer), vim=hx, .. 
+
+# make nushell default
+# todo!
+
