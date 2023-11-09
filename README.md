be6bdac57773b6d217c25333e617020d82766d66
Maxine <maxbeckie@tutanota.com>
2023-11-08 19:28:20 -0800

    readme + symlinks in script

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-dotfiles
+I run `init.fish` to symlink the folders where the program expects its config to be.
---
+++ init.fish
@@ -0,0 +1,9 @@
+# symlink config files:
+
+ln -s alacritty $HOME/.config/alacritty
+ln -s bat $HOME/.config/bat
+ln -s fish $HOME/.config/fish
+ln -s helix $HOME/.config/helix
+ln -s htop $HOME/.config/htop
+
+# TODO: find reasonably secure way to setup git config and stuff
